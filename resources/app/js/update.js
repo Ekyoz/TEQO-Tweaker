846968f3a35198109a7a2af205485a6be9d10911
mizu-cmd <mithanielvillard@gmail.com>
2021-08-29 14:57:46 +0200

    updated update.js

--- resources/app/js/update.js
+++ resources/app/js/update.js
@@ -13,10 +13,10 @@ const defaultStages = {
 
 const updateOptions = {
     gitRepo: "TEQO-Tweaker", // [Required] Your Repo Name
-    gitUsername: "mizu-cmd",  // [Required] Your GitHub Username.
+    gitUsername: "Ekyoz",  // [Required] Your GitHub Username.
 
     appName: "TEQO-Tweaker", //[Required] The Name of the app archive and the app folder.
-    appExecutableName: "TEQO Tweaker.exe", //[Required] The Executable of the Application to be Run after updating.
+    appExecutableName: "TEQO-Tweaker.exe", //[Required] The Executable of the Application to be Run after updating.
 
     progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
     label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
