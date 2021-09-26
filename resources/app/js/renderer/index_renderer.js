2f7fa007bac5ba1818755d5ccb0ea13728b2f67d
alexandre <alexandre.tresallet@gmail.com>
2021-09-26 12:24:24 +0200

    add shadow

--- resources/app/js/renderer/index_renderer.js
+++ resources/app/js/renderer/index_renderer.js
@@ -42,14 +42,14 @@ function checkState(){
     if (message.includes("connected") && $("#connected-img").length == 0){
       $("#disconnected-img").remove()
       $("#disconnected-txt").remove()
-      $("#state").append('<img src="../img/check.png" alt="" style="width: 90px; margin-left: 945px; margin-top: 290px; opacity: 0; transition: 0.24s" id="connected-img">')
+      $("#state").append('<img src="../img/check.png" alt="" style="width: 90px; position: fixed; right: 175px; top: 280px; opacity: 0; transition: 0.24s" id="connected-img">')
       $("#status").append('<p id="connected-txt" style="color: green; font-weight: normal; text-align: center; font-size: 18px; opacity: 0; transition: 0.24s">Connected</p>')
       $("#connected-img").fadeTo(0.24, 1)
       $("#connected-txt").fadeTo(0.24, 1)
     } else if($("#disconnected-img").length == 0){
       $("#connected-img").remove()
       $("#connected-txt").remove()
-      $("#state").append('<img src="../img/bouton-croix.png" alt="" style="width: 90px; margin-left: 945px; margin-top: 290px; opacity: 0; transition: 0.24s" id="disconnected-img">')
+      $("#state").append('<img src="../img/bouton-croix.png" alt="" style="width: 90px; position: fixed; right: 175px; top: 280px; opacity: 0; transition: 0.24s" id="disconnected-img">')
       $("#status").append('<p id="disconnected-txt" style="color: red; font-weight: normal; text-align: center; font-size: 18px; opacity: 0; transition: 0.24s">Disconnected</p>')
       $("#disconnected-img").fadeTo(0.24, 1)
       $("#disconnected-txt").fadeTo(0.24, 1)
